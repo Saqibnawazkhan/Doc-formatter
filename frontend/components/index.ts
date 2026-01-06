8ba9b34fd5ccfedde766436da03cc1c8ee33c1d2
Saqib nawaz Khan <67519982+Saqibnawazkhan@users.noreply.github.com>
2026-01-06 05:24:35 +0500

    Change 11: Update component exports to include all new UI components

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,4 +1,13 @@
+// Core components
 export { default as FileUpload } from './FileUpload';
 export { default as FormattingOptions } from './FormattingOptions';
 export { default as DocumentPreview } from './DocumentPreview';
 export { default as Header } from './Header';
+
+// Preset components
+export { default as QuickPresets } from './QuickPresets';
+
+// UI components
+export { default as Tooltip } from './Tooltip';
+export { default as Skeleton, DocumentSkeleton, CardSkeleton, PresetSkeleton } from './Skeleton';
+export { default as ToastProvider, useToast } from './Toast';
